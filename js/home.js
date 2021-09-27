e70641edb38ff7563e3a1770be9483242d30fa7a
abay-2002 <akbar121202@gmail.com>
2021-09-27 23:04:04 +0800

    menambahkan author pada banner

--- js/home.js
+++ js/home.js
@@ -5,7 +5,10 @@ export default function Home(){
       <div id="banner">
         <div class="banner-text">
         <h1 class="text-center text-wrap" id="welcome-text">Hello World!</h1>
-        <p class="text-center text-wrap">assumenda rerum perferendis commodi quas adipisci veritatis omnis ut.</p>
+        <p class="text-center text-wrap">
+            This site is underconstruction :D
+            by: <a href="http://akbarangkasa.com/" target="blank">abay</a>
+        </p>
         </div>
       </div>
     `);

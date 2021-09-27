d4af9e03887faa6c8337059c39c36bf14d8b8477
abay-2002 <akbar121202@gmail.com>
2021-09-27 22:51:45 +0800

    navbar dan banner DASHBOARD sudah menetapkan js module

---
+++ js/home.js
@@ -0,0 +1,12 @@
+export default function Home(){
+    $('#root').append(`
+    <!-- === DASHBOARD === -->
+      <!-- banner -->
+      <div id="banner">
+        <div class="banner-text">
+        <h1 class="text-center text-wrap" id="welcome-text">Hello World!</h1>
+        <p class="text-center text-wrap">assumenda rerum perferendis commodi quas adipisci veritatis omnis ut.</p>
+        </div>
+      </div>
+    `);
+};
--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 // ES6 Import
 import {default as Navbar} from './navbar.js';
-
+import {default as Home} from './home.js';
 import {default as BootstrapFunction} from './bootstrap.js';
 
 Navbar();
+Home();
 BootstrapFunction();
 

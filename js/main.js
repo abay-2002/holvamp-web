a72f3659ec37b0cfd77be1a3d98159d7b54a5b26
abay-2002 <akbar121202@gmail.com>
2021-09-28 16:12:11 +0800

    fix about us

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-// ES6 Import
-import {default as Navbar} from './navbar.js';
-import {default as Home} from './home.js';
+// // ES6 Import
+// import {default as Navbar} from './navbar.js';
+// import {default as Home} from './home.js';
 import {default as BootstrapFunction} from './bootstrap.js';
 
-Navbar();
-Home();
+// Navbar();
+// Home();
 BootstrapFunction();
 
